0658dc16f6ba99c0623d497b9636e7533651c1bd
SivaMadhavan <sivamadhavan619@gmail.com>
2021-07-27 07:15:27 +0530

    url changes

--- MyApp/urls.py
+++ MyApp/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 from .views import *
+
 urlpatterns = [
     path('',home),
     path('result',result)
